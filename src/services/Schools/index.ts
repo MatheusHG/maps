cb680dd5ea07bcff5ac6932be57a18b0c5d95c60
AmauriLima <amauri.plimaj@gmail.com>
2022-07-04 19:36:42 -0300

    fix: school image

--- src/services/Schools/index.ts
+++ src/services/Schools/index.ts
@@ -11,11 +11,18 @@ interface GetSchoolImageProps {
 class SchoolsService {
   async getSchoolImage(props: Partial<GetSchoolImageProps>) {
     const { codigo_uf, escola, municipio } = props;
+    const { hostname } = window.location;
+
+    const apiURL =
+      hostname === 'localhost'
+        ? 'http://localhost:3333'
+        : 'https://api.pilab.com.br';
+
     try {
       await api.get(
         `maps/photo?codigo_uf=${codigo_uf}&escola=${escola}&municipio=${municipio}`,
       );
-      return `http://localhost:3333/maps/photo?codigo_uf=${codigo_uf}&escola=${escola}&municipio=${municipio}`;
+      return `${apiURL}/maps/photo?codigo_uf=${codigo_uf}&escola=${escola}&municipio=${municipio}`;
     } catch (err) {
       return 'https://user-images.githubusercontent.com/56003521/165186523-9a2207a9-db8e-4a56-9679-270a78e70e0a.png';
     }
