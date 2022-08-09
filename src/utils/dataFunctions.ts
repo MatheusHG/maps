9eeefae434e4ebee9431bd85832302f654c4fb01
AmauriLima <amauri.plimaj@gmail.com>
2022-08-09 11:23:41 -0300

    fix

--- src/utils/dataFunctions.ts
+++ src/utils/dataFunctions.ts
@@ -5,19 +5,6 @@ export function formatNumber(number: number) {
     minimumFractionDigits: 2,
   });
 }
-
 export function removeSpecialChars(string: string) {
-  return string
-    .replace(/[àáâãäå]/, 'a')
-    .replace(/[ÀÁÂÃÄÅ]/, 'A')
-    .replace(/[èéêẽë]/, 'e')
-    .replace(/[ÈÉÊẼË]/, 'E')
-    .replace(/[ìíîĩï]/, 'i')
-    .replace(/[ÌÍÎĨÏ]/, 'I')
-    .replace(/[òóôõö]/, 'o')
-    .replace(/[ÒÓÔÕÖ]/, 'O')
-    .replace(/[ùúûũü]/, 'u')
-    .replace(/[ÙÚÛŨÜ]/, 'U')
-    .replace(/[ç]/, 'c')
-    .replace(/[Ç]/, 'C');
+  return string.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
